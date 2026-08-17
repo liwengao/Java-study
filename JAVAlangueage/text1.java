32e2b08d96fd50c5848c4b9a728c4c7375addd70
liwengao <2806362136@qq.com>
2026-08-17 18:27:15 +0800

    2

---
+++ JAVAlangueage/text1.java
@@ -0,0 +1,8 @@
+package JAVAlangueage;
+
+public class text1 {
+    static void main(String[] args) {
+
+        System.out.println("hello world");
+    }
+}
--- java语法.java
+++
@@ -1,5 +0,0 @@
-public class java语法 {
-    static void main(String[] args) {
-        System.out.println("hello worl);
-    }
-}
