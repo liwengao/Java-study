e2981413ca78f66207669f8eb32416cca4fa99b3
liwengao <2806362136@qq.com>
2026-08-21 02:41:45 +0800

    1

---
+++ 01java-basic/src/basicLanguage/Text8_VariadicParameter.java
@@ -0,0 +1,22 @@
+package basicLanguage;
+
+public class Text8_VariadicParameter {
+    public static void main(String[] args) {
+        System.out.println(add(1,2,3,4,5));
+        System.out.println(aaa(1,1,1,6,1,1));
+
+    }
+    public static  int add(int... nums){
+        int sum=0;
+        for(int i=0;i< nums.length;i++)
+        {
+            sum+=nums[i];
+        }
+        return sum;
+    }
+    public static  int aaa(int a,int... nums){
+        int sum=0;
+        sum=a+nums.length;
+        return sum;
+    }
+}
