68dbdea9c4d81a1a169e8b5d21795f7d8889d5a8
liwengao <2806362136@qq.com>
2026-08-23 01:43:03 +0800

    1

--- 02object-oriented/src/chap03/PhysicalProduct.java
+++ 02object-oriented/src/chap03/PhysicalProduct.java
@@ -1,4 +1,4 @@
-package chap3;
+package chap03;
 
 public class PhysicalProduct extends Product {
     public double getWeight() {
@@ -8,13 +8,20 @@ public class PhysicalProduct extends Product {
     public PhysicalProduct(String name,double price,double weight) {
         super(name, price);//Java中隐含调用父辈
         this.weight = weight;
-        System.out.println("PhysicalProduct()");
+//        System.out.println("PhysicalProduct()");
     }
-
+    public String displayInfo()
+    {
+        return super.displayInfo()+",重量:"+weight;
+    }
     public void setWeight(double weight) {
         this.weight = weight;
     }
 
     private double weight;
 
+    public void sendProduct(){
+        System.out.println("发送实体商品：" + super.getName());
+
+    }
 }
--- 02object-oriented/src/chap03/Product.java
+++ 02object-oriented/src/chap03/Product.java
@@ -1,24 +1,33 @@
-package chap3;
+package chap03;
 
-public class Product extends Grandpa{
+public abstract class Product {
     private String name;
     private double price;
     public Product(String name, double price) {
         this.name = name;
         this.price = price;
-        System.out.println("Product()");
+//        System.out.println("Product()");
+    }
+    public static PhysicalProduct createPhysicalProduct(String name, double price, double weight) {
+        return new PhysicalProduct(name, price, weight);
+    }
+    public static DigitalProduct createDigitalProduct(String name, double price, String secretkey) {
+        return new DigitalProduct(name, price, secretkey);
     }
     public String displayInfo()
     {
         return "商品名称：" + name + "，价格：" + price;
-    }
 
+    }
     public void setName(String name) {
         this.name = name;
     }
-
+    public String getName() {
+        return name;
+    }
     public void setPrice(double price) {
         this.price = price;
     }
+    public abstract void sendProduct();
 
 }
